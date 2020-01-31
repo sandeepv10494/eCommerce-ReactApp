cb1d35b10f4cc8602b8bdb9142c88c61de336ee6
unknown <Sandeep.V@yethi.in>
2020-01-31 15:42:18 +0530

    Updating Sign IN

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 
@@ -11,8 +11,6 @@ import { auth , createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-  
 
   unsubscribeFromAuth = null;
 
@@ -47,15 +45,19 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path ='/shop' component={ShopPage}></Route>
-        <Route path ='/signin' component={SignInAndSignUpPage}></Route>
+        <Route exact path ='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>):(<SignInAndSignUpPage />)}></Route>
        </Switch>
     </div>
   );
   }  
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
