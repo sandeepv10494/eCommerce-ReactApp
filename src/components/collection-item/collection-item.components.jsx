cfa50191144978775b5d004b97a4c5222920ad7c
unknown <Sandeep.V@yethi.in>
2020-02-01 19:35:50 +0530

    Added add to cart item action

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -1,9 +1,13 @@
 import React from 'react' ;
-
+import { connect } from 'react-redux';
 import './collection-item.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
+import CustomButton from '../custom-button/custom-button.components';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+    return (
     <div className='collection-item'>
         <div className='image'
             style={{
@@ -13,9 +17,15 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <div className='collection-footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>${price}</span>
+               
             </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to Cart</CustomButton>
         </div>
-    
-);
+    )
+        };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
