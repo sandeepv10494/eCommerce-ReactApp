dda4f601521d5e1faa0291a1ff6e788f4e34a2a2
unknown <Sandeep.V@yethi.in>
2020-02-03 16:24:22 +0530

    Created cart item component

--- src/components/collection-item/collection-item.components.jsx
+++ src/components/collection-item/collection-item.components.jsx
@@ -2,7 +2,7 @@ import React from 'react' ;
 import { connect } from 'react-redux';
 import './collection-item.styles.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import CustomButton from '../custom-button/custom-button.components';
+import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
 const CollectionItem = ({item, addItem}) => {
